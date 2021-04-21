03439eb2cebac2b443f5079dcbd8abfb1460abd9
Chytra K R <34940260+chytrakr@users.noreply.github.com>
2021-04-21 15:24:05 +0530

    Update file.vue

--- src/vue/file.vue
+++ src/vue/file.vue
@@ -11,7 +11,7 @@
       :aria-invalid="question.errors.length > 0"
       :aria-describedby="question.errors.length > 0 ? question.id + '_errors' : null"  
       :multiple="question.allowMultiple ? 'multiple' : undefined"
-      v-bind:title="question.inputTitle"
+      v-bind:title="question.inputTitle + '..'"
       v-bind:accept="question.acceptedTypes"
     />
     <input
